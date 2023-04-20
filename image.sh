5c5ab491cdb9b77c724c04fb750c6be0fee9e5dc
brandongrier7 <bgrier@udel.edu>
2023-04-19 21:25:30 -0400

    batch added

---
+++ image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --job-name=output.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output output.txt-job_%j.out
+#SBATCH --error output.txt-job_%j.err
+#SBATCH --partition=standard
+
+# Bind your OpenMP threads
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Start my application
+srun ./omp-image
+srun ./pthread-image
