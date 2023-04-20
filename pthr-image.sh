3709fd33349ab918119ce5fdb77d5adc0404fc7b
brandongrier7 <bgrier@udel.edu>
2023-04-19 21:54:15 -0400

    last commit hopefully

--- pthr-image.sh
+++ pthr-image.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -l
-#SBATCH --job-name=omp-output.txt
+#SBATCH --job-name=pthr-output.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --output output.txt-job_%j.out
 #SBATCH --error output.txt-job_%j.err
 #SBATCH --partition=standard
 
-# Bind your OpenMP threads
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-
 # Start my application
-srun omp-image pic1.jpg edge
+srun pthread-image pic1.jpg edge
